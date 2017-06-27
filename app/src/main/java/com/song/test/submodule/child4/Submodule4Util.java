b97476ef81ff475c34e110d890d4ddd043fab1a2
Banton <1039935805@qq.com>
2017-06-27 15:25:44 +0800

    add file

---
+++ app/src/main/java/com/song/test/submodule/child4/Submodule4Util.java
@@ -0,0 +1,12 @@
+package com.song.test.submodule.child4;
+
+import android.util.Log;
+
+/**
+ * Created by songyawei on 2017/6/26.
+ */
+public class Submodule4Util {
+    public static void test() {
+        Log.e(Submodule4Util.class.getSimpleName(), "Submodule4Util");
+    }
+}
